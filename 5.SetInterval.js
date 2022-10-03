43abc900d815ae7feac9282c6834bd8443d65aef
Bibhuprasad Harichandan Sahu <bibhuprasad035@gmail.com>
2022-10-03 07:43:17 +0530

    setinterval method for repeating

---
+++ 5.SetInterval.js
@@ -0,0 +1,22 @@
+// program to display a text using setInterval method
+function greet() {
+    console.log('Hello world');
+}
+
+setInterval(greet, 1000);
+
+
+
+// program to display time every 5 seconds
+function showTime() {
+
+    // return new date and time
+    let dateTime= new Date();
+
+    // return the time
+    let time = dateTime.toLocaleTimeString();
+
+    console.log(time)
+}
+
+let display = setInterval(showTime, 5000);
